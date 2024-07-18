b0a63ce1607b885a2ef3e43345f5bce9268f0eef
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-07-18 20:31:33 +0200

    chore(lint): update vue block order rule

--- frontend/components/ButtonCard.vue
+++ frontend/components/ButtonCard.vue
@@ -1,17 +1,14 @@
+<template>
+  <NuxtLink :to="to" class="block">
+    <button class="w-full h-full hover:bg-gray-100 rounded p-10 border border-gray-400 bg-gray-50 flex justify-center items-center">
+      <slot />
+    </button>
+  </NuxtLink>
+</template>
+
 <script lang="ts" setup>
 interface Props {
   to?: string
 }
 defineProps<Props>()
 </script>
-
-<template>
-  <NuxtLink
-    :to="to"
-    class="block"
-  >
-    <button class="w-full h-full hover:bg-gray-100 rounded p-10 border border-gray-400 bg-gray-50 flex justify-center items-center">
-      <slot />
-    </button>
-  </NuxtLink>
-</template>
--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -4,4 +4,10 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default await withNuxt(
   antfu(),
-)
+).override('antfu/vue/rules', {
+  rules: {
+    'vue/block-order': ['error', {
+      order: ['template', 'script', 'style'],
+    }],
+  },
+})
