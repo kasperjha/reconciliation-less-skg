5b4a80107bd110369794efc5d40737ece9c7cd77
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-07-19 04:23:35 +0200

    refactor: adjust spacing

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="w-full h-[100dvh] flex flex-col items-center p-2">
+  <div class="w-full h-[100dvh] flex flex-col items-center p-2 overflow-x-scroll">
     <div class="max-w-xl w-full">
       <AppBreadcrumbs class="mr-auto py-8" />
       <NuxtPage />
+      <footer class="m-20" />
     </div>
   </div>
 </template>
