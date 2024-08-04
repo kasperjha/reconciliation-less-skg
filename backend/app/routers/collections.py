880fdf7d2ed0bffd98ba94f4097dab9e6784fb6b
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-08-04 20:45:13 +0200

    refactor: use repository and improve error handling

--- backend/app/routers/collections.py
+++ backend/app/routers/collections.py
@@ -1,14 +1,13 @@
-from pathlib import Path
+from http.client import CONFLICT
 from fastapi import APIRouter, HTTPException, UploadFile
 from pydantic import BaseModel
 
-
-# TODO: use a database
-collections = []
-collections.append(
-    {"id": 1, "name": "Collection A", "datasets": [{"filename": "oliviera-car.csv"}]}
-)  # type: ignore
-collections.append({"id": 2, "name": "Collection B", "datasets": []})  # type: ignore
+from app.repository.collections import (
+    CollectionCreate,
+    CollectionNotFoundException,
+    TestCollectionRepository,
+)
+from app.services.ingestion import IngestionService, MissingFilenameError
 
 
 router = APIRouter(prefix="/collections", tags=["collections"])
@@ -18,71 +17,49 @@ class Dataset(BaseModel):
     filename: str
 
 
-class CollectionCreate(BaseModel):
-    name: str
-
-
 class Collection(BaseModel):
     id: int
     name: str
     datasets: list[Dataset]
 
 
+collections = TestCollectionRepository()
+
+
 @router.get("/")
-def get_collections() -> list[Collection]:
-    return collections
+def get_collections():
+    return collections.get_all()
 
 
-@router.post("/collections/")
+@router.post("/")
 def create_collection(collection: CollectionCreate) -> Collection:
-    newCollection: Collection = {
-        "id": len(collections) + 1,
-        "name": collection.name,
-        "datasets": [],
-    }  # type: ignore
-
-    collections.append(newCollection)
-    return newCollection
+    return collections.create(collection)
 
 
-@router.get("/{collection_id}")
-def get_collection(collection_id: int):
-    collection = next(filter(lambda c: c["id"] == collection_id, collections), None)
-
-    if collection is None:
-        raise HTTPException(status_code=404, detail="Collection not found.")
-
+@router.get("/{id}")
+def get_collection(id: int):
+    try:
+        collection = collections.get_by_id(id)
+    except CollectionNotFoundException:
+        raise HTTPException(404, "Collection not found.")
     return collection
 
 
-@router.post("/{collection_id}/datasets")
-def upload_datasets(collection_id: int, files: list[UploadFile]):
-    collection = next(filter(lambda c: c["id"] == collection_id, collections), None)
+@router.post("/{id}/datasets")
+def upload_datasets(id: int, files: list[UploadFile]):
+    ingestion = IngestionService(collections)
 
-    if collection is None:
-        raise HTTPException(status_code=404, detail="Collection not found.")
-
-    datasets = Path("datasets")
-    for file in files:
-        if file.filename is None:
-            raise HTTPException(status_code=400, detail="File without filename.")
-
-        if datasets.joinpath(file.filename).exists():
-            raise HTTPException(
-                status_code=409,
-                detail=f"Dataset with name '{file.filename}' already exists.",
-            )
-
-        # TODO: validate the format of the dataset
-
-    for dataset in files:
-        filename = dataset.filename
-        assert filename is not None
-        with open(datasets.joinpath(filename), "wb") as file:
-            content = dataset.file.read()
-            file.write(content)
-
-    datasets = [{"filename": file.filename} for file in files]
-    collection["datasets"].extend(datasets)
+    try:
+        for file in files:
+            ingestion.ingest_dataset(id, file)
+    except CollectionNotFoundException:
+        raise HTTPException(404, "Collection not found")
+    except MissingFilenameError:
+        raise HTTPException(400, "A file is missing a filename")
+    except FileExistsError:
+        raise HTTPException(
+            status_code=409,
+            detail=f"Dataset named '{file.filename}' already exists for collection.",
+        )
 
     return {"message": "Files processed sucessfully."}
