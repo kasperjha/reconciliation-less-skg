9e9b476e058f6a3f42e9429ab650226054bf6828
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-08-04 23:09:45 +0200

    refactor: extract helpers

--- backend/app/repository/datasets.py
+++ backend/app/repository/datasets.py
@@ -13,25 +13,31 @@ class DatasetsRepository:
         if not self.dataset_dir.exists():
             raise FileNotFoundError("Datasets folder does not exist.")
 
+    def _build_collection_dir(self, collection_id):
+        return self.dataset_dir.joinpath(str(collection_id))
+
+    def _build_dataset_path(self, collection_id, dataset_filename):
+        return self._build_collection_dir(collection_id).joinpath(dataset_filename)
+
     def create(self, collection_id: int, file: UploadFile):
         """Persists a dataset at the local file system."""
         if file.filename is None:
             raise MissingFilenameError()
 
         # make sure collection and directory exists
-        collection_dataset_dir = self.dataset_dir.joinpath(str(collection_id))
-        collection_dataset_dir.mkdir(exist_ok=True)
+        collection_dir = self._build_collection_dir()
+        collection_dir.mkdir(exist_ok=True)
 
         # make sure datset name is unique for collection
-        new_path = collection_dataset_dir.joinpath(file.filename)
+        dataset_path = self._build_dataset_path()
 
-        if new_path.exists():
+        if dataset_path.exists():
             raise FileExistsError()
 
         # TODO: also validate that dataset has appropriate format
 
         # write to file system
-        with open(new_path, "wb") as dstfile:
+        with open(dataset_path, "wb") as dstfile:
             content = file.file.read()  # TODO: chuck file upload for large files
             dstfile.write(content)
 
