35f70172e218cefe82688376c7b4d2da464cc0a2
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-08-04 16:24:03 +0200

    refactor: extract /analysis router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from backend.app.services.analysis import mean_std_quantization
-from .routers import collections
+from .routers import collections, analysis
 
 app = FastAPI()
 
@@ -17,23 +16,4 @@ app.add_middleware(
 )
 
 app.include_router(collections.router)
-
-
-@app.get("/proto/analysis")
-def proto_analysis() -> str:
-    datasets = [
-        "oliviera-car.csv",
-        "oliviera-los-far.csv",
-        "oliviera-los-near.csv",
-        "oliviera-nlos.csv",
-        "oliviera-walking.csv",
-    ]
-
-    result = ""
-    for dataset in datasets:
-        result += "\n"
-        result += "".join(map(lambda x: str(x), mean_std_quantization(gw[0:100])))
-        result += "".join(map(lambda x: str(x), mean_std_quantization(node[0:100])))
-        result += "\n"
-
-    return {result: result}
+app.include_router(analysis.router)
---
+++ backend/app/routers/analysis.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+
+from backend.app.services.analysis import mean_std_quantization
+
+
+router = APIRouter(prefix="/analysis", tags=["analysis"])
+
+
+@router.get("/prototype")
+def proto_analysis() -> str:
+    datasets = [
+        "oliviera-car.csv",
+        "oliviera-los-far.csv",
+        "oliviera-los-near.csv",
+        "oliviera-nlos.csv",
+        "oliviera-walking.csv",
+    ]
+
+    result = ""
+    for dataset in datasets:
+        result += "\n"
+        result += "".join(map(lambda x: str(x), mean_std_quantization(gw[0:100])))
+        result += "".join(map(lambda x: str(x), mean_std_quantization(node[0:100])))
+        result += "\n"
+
+    return {result: result}
