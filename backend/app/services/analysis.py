c5e5b21c140fd332ab06adaf7c54ac8db9a46505
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-08-05 11:54:19 +0200

    feat: use suite in analysis service

--- backend/app/services/analysis.py
+++ backend/app/services/analysis.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from app.repository.collections import CollectionsRepository
 from app.repository.datasets import DatasetsRepository
 from app.services.algorithms import MeanStdQuantiser
+from app.services.randomness import NistRandomnessAnalyser
 
 
 class AnalysisOptions(BaseModel):
@@ -15,14 +16,23 @@ class AnalysisService:
         self.collections = collections
         self.datasets = datasets
         self.quantiser = MeanStdQuantiser()
+        self.randomness = NistRandomnessAnalyser()
 
     def analyse_collection(self, options: AnalysisOptions):
         collection = self.collections.get_by_id(options.collection_id)
         filenames = [dataset.filename for dataset in collection.datasets]
 
         for filename in filenames:
-            gateway, node = self.datasets.get(options.collection_id, filename)
-            quantised_gateway = self.quantiser.quantise(gateway)
-            quantised_node = self.quantiser.quantise(node)
 
-            raise NotImplementedError()
+            print("dataset:", filename)
+
+            gateway, node = self.datasets.get(options.collection_id, filename)
+            gw_quantised = self.quantiser.quantise(gateway)
+            gw_randomeness = self.randomness.analyse_key_randomness(gw_quantised)
+
+            print("gw_random?:", gw_randomeness)
+
+            node_quantised = self.quantiser.quantise(node)
+            node_randomness = self.randomness.analyse_key_randomness(node_quantised)
+
+            print("node_random?:", node_randomness)
