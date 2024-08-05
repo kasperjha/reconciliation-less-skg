23291c85befa5868021ae8851ff7ab44db3ba2cd
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-08-05 18:03:29 +0200

    feat: define analysis response structure

--- backend/app/services/analysis.py
+++ backend/app/services/analysis.py
@@ -1,7 +1,41 @@
+from pydantic import BaseModel
 from app.repository.collections import CollectionsRepository
 from app.repository.datasets import DatasetsRepository
 from app.services.algorithms import MeanStdQuantiser
 from app.services.randomness import NistRandomnessAnalyser
+
+
+# class RandomnessTestResult(BaseModel):
+#     test: str
+#     passed: bool
+#     p_value: float
+
+
+# class RandomnessAnalysisResult(BaseModel):
+#     tests_total: int
+#     tests_passed: int
+#     results: list[RandomnessTestResult]
+
+
+class NoDatasetsError(Exception):
+    pass
+
+
+class AnalysisResultSamples(BaseModel):
+    samples_raw: list[int]
+    samples_processed: list[int]
+    secret_key: str
+    key_randomness_proto: bool
+
+
+class AnalysisResultDataset(BaseModel):
+    filename: str
+    gateway: AnalysisResultSamples
+    node: AnalysisResultSamples
+
+
+class AnalysisResultCollection(BaseModel):
+    results: list[AnalysisResultDataset]
 
 
 class AnalysisService:
@@ -12,21 +46,35 @@ class AnalysisService:
         self.quantiser = MeanStdQuantiser()
         self.randomness = NistRandomnessAnalyser()
 
+    def _analyse_samples(self, raw_samples: list[int]):
+        samples_processed = raw_samples  # TODO: implement preprocessing
+        samples_quantised = self.quantiser.quantise(samples_processed)
+        results = {
+            "samples_raw": raw_samples,
+            "samples_processed": samples_processed,
+            "secret_key": samples_quantised,
+            "key_randomness_proto": self.randomness.analyse_key_randomness(samples_quantised),
+        }
+        return AnalysisResultSamples(**results)
+
+    def _analyse_dataset(self, id, filename: str):
+        gateway, node = self.datasets.get(id, filename)
+        results = {
+            "filename": filename,
+            "gateway": self._analyse_samples(gateway),
+            "node": self._analyse_samples(node),
+        }
+        return AnalysisResultDataset(**results)
+
     def analyse_collection(self, id: int):
         collection = self.collections.get_by_id(id)
         filenames = [dataset.filename for dataset in collection.datasets]
 
+        if not collection.datasets:
+            raise NoDatasetsError()
+
+        results = []
         for filename in filenames:
+            results.append(self._analyse_dataset(id, filename))
 
-            print("dataset:", filename)
-
-            gateway, node = self.datasets.get(id, filename)
-            gw_quantised = self.quantiser.quantise(gateway)
-            gw_randomeness = self.randomness.analyse_key_randomness(gw_quantised)
-
-            print("gw_random?:", gw_randomeness)
-
-            node_quantised = self.quantiser.quantise(node)
-            node_randomness = self.randomness.analyse_key_randomness(node_quantised)
-
-            print("node_random?:", node_randomness)
+        return AnalysisResultCollection(**{"results": results})
