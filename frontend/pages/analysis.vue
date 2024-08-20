866d2fa3bd4999e158694759bc8212ef344f08e8
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-08-20 14:26:14 +0200

    feat: analysis with validation and results

--- frontend/pages/analysis.vue
+++ frontend/pages/analysis.vue
@@ -2,50 +2,47 @@
   <main>
     <h1>Analysis</h1>
 
-    <h2>Choose datasets to analyse</h2>
-    <CollectionPicker v-model="collectionId" />
+    <h2>Choose collection to analyse</h2>
 
-    <USelectMenu
-      v-model="selectedDatasets"
-      :options="rows"
-      option-attribute="filename"
-      value-attribute="filename"
-      placeholder="Select datasets to analyse ..."
-      size="lg"
-      multiple
-    />
-
-    <div class="mt-4 flex gap-2 items-center">
-      <UButton @click="onAnalysisClick">
-        Analyse
+    <div class="flex gap-1">
+      <CollectionPicker v-model="collectionId" class="flex-grow" />
+      <UButton
+        color="white"
+        size="lg"
+        icon="i-heroicons-arrow-top-right-on-square-20-solid"
+        trailing
+        @click="navigateTo('/collections')"
+      >
+        Manage collections
       </UButton>
-      <p class="text-red-600">
-        <span v-if="selectedDatasets.length < 1">
-          Please choose at least one dataset
-        </span>
-      </p>
     </div>
 
-    <UCard>
-      <p>Datasets</p>
-      <code><pre>{{ selectedDatasets }}</pre></code>
-      <p>collection</p>
-      <code><pre>
-        {{ collectionId }}
-      </pre></code>
+    <ContainerRounded class="mt-2">
+      <div v-if="validationError" class="text-gray-600 h-40 flex justify-center gap-1 items-center">
+        <Icon name="i-heroicons-shield-exclamation" class="size-5" />
+        <p>{{ validationError }}</p>
+      </div>
+      <UTable v-else :rows="datasets" />
+    </ContainerRounded>
+
+    <UButton
+      :disabled="validationError !== null"
+      size="md"
+      @click="onAnalysisClick"
+    >
+      Analyse
+    </UButton>
+
+    <h2>Results</h2>
+
+    <AnalysisResult v-if="analysis" :results="analysis.results" />
+
+    <UCard v-if="analysis">
+      <details>
+        <summary>Raw analysis data</summary>
+        <code><pre>{{ analysis }}</pre></code>
+      </details>
     </UCard>
-
-    <h2 class="mt-8">
-      Results
-    </h2>
-
-    <CardFetchResult :status="status" class="overflow-x-scroll">
-      <code>
-        <pre>
-          {{ analysis.result }}
-        </pre>
-      </code>
-    </CardFetchResult>
   </main>
 </template>
 
@@ -53,12 +50,24 @@
 const collectionId = ref(null)
 const collectionUrl = computed(() => `http://127.0.0.1:8000/collections/${collectionId.value}`)
 
-const { data: datasets } = useFetch(collectionUrl, { immediate: false })
-const rows = computed(() => datasets.value ? datasets.value.datasets : [])
-const selectedDatasets = ref([])
+const { data: collection } = useFetch(collectionUrl, { immediate: false })
+const datasets = computed(() => collection.value !== null ? collection.value.datasets : [])
 
-const { data: analysis, execute, status } = useFetch(`http://127.0.0.1:8000/proto/analysis`, { immediate: false })
+const validationError = computed<string | null>(() => {
+  if (datasets.value.length < 1) {
+    return 'Select a collection with at least one dataset'
+  }
+  else {
+    return null
+  }
+})
+
+const analysis = ref<null | object>(null)
+const analysisUrl = computed(() => `http://127.0.0.1:8000/collections/${collectionId.value}/analyse`)
+
 function onAnalysisClick() {
-  execute()
+  $fetch<object>(analysisUrl.value, { method: 'POST' })
+    .then(data => analysis.value = data)
+    .catch(error => console.log(error))
 }
 </script>
