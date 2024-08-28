e37b188117f1b84d390dd56d02d40d5737a08784
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-08-28 15:42:29 +0200

    feat: savitzky-golay filter

--- backend/app/services/algorithms.py
+++ backend/app/services/algorithms.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod, ABC
 from pydantic import BaseModel
 from scipy.stats import pearsonr
+from scipy.signal import savgol_filter
 
 
 def std_dev(samples):
@@ -46,6 +47,15 @@ class Preprocessor(ABC):
     @abstractmethod
     def run(self, samples: list[int]) -> list[int]:
         pass
+
+
+class SGPreprocessor(Preprocessor):
+    def __init__(self, window_length=13, polyorder=3):
+        self.window_length = window_length
+        self.polyorder = polyorder
+
+    def run(self, samples):
+        return savgol_filter(samples, window_length=self.window_length, polyorder=self.polyorder)
 
 
 class SchemeAnalysisResult(BaseModel):
